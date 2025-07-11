e33e44295ede1cbc005d7a00064d0a07dde63397
Jared Wolff <hello@jaredwolff.com>
2025-07-11 09:07:44 -0400

    Add 7zip-bin to external so it doesn't get bundled

--- esbuild.js
+++ esbuild.js
@@ -13,7 +13,7 @@ async function main() {
     sourcesContent: false,
     platform: 'node',
     outfile: 'dist/extension.js',
-    external: ['vscode'],
+    external: ['vscode', '7zip-bin'],
     logLevel: 'silent',
     plugins: [
       /* add to the end of plugins array */
